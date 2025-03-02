2154d4fd385c14b0f0d98467d27af564ee30b739
Pavel <pavelpisligin@yandex.ru>
2025-03-02 15:32:08 +0300

    test

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"bytes"
-	"encoding/gob"
+	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"math/rand"
+	"net/http"
 	"time"
 
+	"github.com/gorilla/mux"
 	"github.com/lovoo/goka"
 )
 
@@ -24,26 +26,53 @@ type Codec interface {
 	Decode(data []byte) (value interface{}, err error)
 }
 
-func Encode(value interface{}) (data []byte, err error) {
-	buf := new(bytes.Buffer)
-	enc := gob.NewEncoder(buf)
-	if err := enc.Encode(value); err != nil {
-		return nil, err
-	}
+type userLikeCodec struct{}
 
-	return buf.Bytes(), nil
+func (ul *userLikeCodec) Encode(value interface{}) (data []byte, err error) {
+	if _, isUserLike := value.(*UserLike); !isUserLike {
+		return nil, fmt.Errorf("тип должен быть *UserLike, получен %T", value)
+	}
+	return json.Marshal(value)
 }
 
-func Decode(data []byte) (value interface{}, err error) {
-	var v interface{}
-	buf := new(bytes.Buffer)
-	buf.Write(data)
-	dec := gob.NewDecoder(buf)
-	if err := dec.Decode(&v); err != nil {
-		return nil, err
+func (ul *userLikeCodec) Decode(data []byte) (any, error) {
+	var (
+		userLike UserLike
+		err      error
+	)
+	err = json.Unmarshal(data, &userLike)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка десериализации: %v", err)
 	}
+	return &userLike, nil
+}
 
-	return v, nil
+type UserPost struct {
+	PostLike map[int]bool
+}
+
+// userPostCodec позволяет сериализовать и десериализовать UserPost для групповой таблицы.
+type userPostCodec struct{}
+
+// Encode переводит UserPost в []byte
+func (up *userPostCodec) Encode(value any) ([]byte, error) {
+	if _, isUserPost := value.(*UserPost); !isUserPost {
+		return nil, fmt.Errorf("тип должен быть *UserPost, получен %T", value)
+	}
+	return json.Marshal(value)
+}
+
+// Decode переводит UserPost из []byte в структуру.
+func (up *userPostCodec) Decode(data []byte) (any, error) {
+	var (
+		userPost UserPost
+		err      error
+	)
+	err = json.Unmarshal(data, &userPost)
+	if err != nil {
+		return nil, fmt.Errorf("ошибка десериализации: %v", err)
+	}
+	return &userPost, nil
 }
 
 func runEmitter() {
@@ -71,4 +100,74 @@ func runEmitter() {
 			log.Fatal(err)
 		}
 	}
+}
+
+// ProcessCallback function is called for every message received by the
+// processor.
+type ProcessCallback func(ctx goka.Context, msg interface{})
+
+func process(ctx goka.Context, msg any) {
+	var userLike *UserLike
+	var ok bool
+	var userPost *UserPost
+
+	if userLike, ok = msg.(*UserLike); !ok || userLike == nil {
+		return
+	}
+
+	if val := ctx.Value(); val != nil {
+		userPost = val.(*UserPost)
+	} else {
+		userPost = &UserPost{PostLike: make(map[int]bool)}
+	}
+
+	userPost.PostLike[userLike.PostId] = userLike.Like
+
+	ctx.SetValue(userPost)
+	log.Printf("[proc] key: %s,  msg: %v, data in group_table %v \n", ctx.Key(), userLike, userPost)
+}
+
+// runProcessor обрабатывает сообщения из топиков кафка
+func runProcessor() {
+	g := goka.DefineGroup(group,
+		goka.Input(topic, new(userLikeCodec), process),
+		goka.Persist(new(userPostCodec)),
+	)
+	p, err := goka.NewProcessor(brokers,
+		g,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = p.Run(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func runView() {
+	view, err := goka.NewView(brokers,
+		goka.GroupTable(group),
+		new(userPostCodec),
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/{key}", func(w http.ResponseWriter, r *http.Request) {
+		value, _ := view.Get(mux.Vars(r)["key"])
+		data, _ := json.Marshal(value)
+		w.Write(data)
+	})
+	fmt.Println("View opened at http://localhost:9095/")
+	go http.ListenAndServe(":9095", router)
+
+	view.Run(context.Background())
+}
+
+func main() {
+	go runEmitter()
+	go runProcessor()
+	runView()
 }
